14561996d190eb2e78ecaabb53b222a6502f32fb
umer farooq <umerfarooq1105@gmail.com>
2023-05-21 10:23:46 +0500

    something

--- Uni stuff/PF/PF-Lab/PF-LAB07/22i-0518_CY-A_Task5.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int age;
-	cout<<"Enter age";
-	cin>>age;
-	(age>18)? cout<<"The user can vote" : cout<<"The user is not yet old enough to vote";
-
-}
