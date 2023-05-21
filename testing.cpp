0033b85de2c8f17e752fd6041a3306a76ca2c5b5
umer farooq <umerfarooq1105@gmail.com>
2023-05-21 10:24:22 +0500

    something

--- testing.cpp
+++ testing.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
+class x
+{
+    int d;
+    public:
+    x(int l=5):d(l){}
+    friend void operator >>(ostream& lol,x& obj);
+};
+void operator >>(ostream& lol,x& obj)
+{
+    lol<<obj.d;
+}
+
+
 int main()
 {
-    int* c = new int;
-   int* & a = c;
-   int& *b;
+    x saifi;
+    cout>>saifi;
+
+}
+int fact(int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
 }
